011682e1345bb353effc06b19a5b3352c76d2b35
hadizz <hadizareoriginal@gmail.com>
2020-04-23 02:17:17 +0430

    upgrade rodcut@dynamic

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import ir.hadiz.dynamic.Rodcut;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 5, 8, 9, 10, 17, 17, 20};
+
         new Rodcut(arr).start();
     }
 }
--- src/ir/hadiz/dynamic/Rodcut.java
+++ src/ir/hadiz/dynamic/Rodcut.java
@@ -1,33 +1,95 @@
 package ir.hadiz.dynamic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+// this solution shows us the maximum price and the way.
 
 public class Rodcut {
-    int[] p, x;
-    int length;
+    private int[] p, r, x;
 
     public Rodcut(int[] p) {
-        this.p = p;
-        this.length = p.length;
-        this.x = new int[p.length];
+        this.p = func(p);
+        this.x = new int[p.length+1];
+        this.r = new int[p.length+1];
+    }
+
+    private int[] func(int[] p) {
+        int[] pp = new int[p.length+1];
+        System.arraycopy(p, 0, pp, 1, pp.length - 1);
+        return pp;
     }
 
     public void start() {
-        System.out.println(Arrays.toString(p));
-        int result = rodcut(length);
-        System.out.println("result is : " + result);
+        System.out.println("prices :" + Arrays.toString(p));
+        System.out.println("result is : " + rodcut(p.length - 1));
+        System.out.println("result of i :" + Arrays.toString(r));
+        System.out.println("selected items :" + Arrays.toString(x));
+        System.out.println("selected items are -> "+print(p.length - 1));
     }
 
+    // 0 1 3 4 5
     private int rodcut(int n) {
+        int max, index;
+        for (int i = 1; i <= n; i++) {
+            max = -1;
+            index = 0;
+            for (int j = 1; j <= i; j++) {
+                int result = p[j] + r[i - j];
+                if (result > max) {
+                    max = result;
+                    index = j;
+                }
+
+            }
+            r[i] = max;
+            x[i] = index;
+        }
+
+        return r[n];
+    }
+
+    private String print(int n) {
         if (n <= 0)
-            return 0;
-
-        int max_val = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++)
-            max_val = Math.max( max_val, p[i] + rodcut(n - i - 1) );
-
-        return max_val;
+            return "";
+        return x[n] + ":" + print(n - x[n]);
     }
 
 }
+
+// this solution only shows us the maximum price
+
+//package ir.hadiz.dynamic;
+//
+//        import java.util.Arrays;
+//
+//public class Rodcut {
+//    int[] p, x;
+//    int length;
+//
+//    public Rodcut(int[] p) {
+//        this.p = p;
+//        this.length = p.length;
+//        this.x = new int[p.length];
+//    }
+//
+//    public void start() {
+//        System.out.println(Arrays.toString(p));
+//        int result = rodcut(length);
+//        System.out.println("result is : " + result);
+//    }
+//
+//    private int rodcut(int n) {
+//        if (n <= 0)
+//            return 0;
+//
+//        int max_val = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < n; i++)
+//            max_val = Math.max( max_val, p[i] + rodcut(n - i - 1) );
+//
+//        return max_val;
+//    }
+//
+//}
